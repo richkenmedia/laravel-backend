77d69da92bd49c3b6664fdd26e29bc96f54845e1
Shine S <shineklbm@gmail.com>
2015-09-26 11:28:50 +0000

    initial commit with Laravel+bootstrap+sentinel

---
+++ app/Http/Middleware/SentinelMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Sentinel;
+
+class SentinelMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($user = Sentinel::check())
+        {
+            return $next($request);
+        }
+        else
+        {
+            echo "You are restricted!";
+        }
+        
+    }
+}
