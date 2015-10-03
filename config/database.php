8e50a837d0997a05adf4083cc6fe5ec8332be490
Shine Sudarsanan <shine@emvigotech.com>
2015-10-03 12:38:23 +0530

    updated the db file

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
             'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'laravel'),
             'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', 'home698867'),
+            'password'  => env('DB_PASSWORD', 'myDBPassword'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
